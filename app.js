1034eebd05db5da979f1d88cdc6d0a4eabecff67
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-04-27 21:19:48 +0100

    Connect to DB, Load default items from DB done.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
 app.set("view engine", "ejs") //Tell your app to use ejs as the view engine. Must be under the app declaration
@@ -7,20 +8,50 @@ app.set("view engine", "ejs") //Tell your app to use ejs as the view engine. Mus
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-let items = []
+mongoose.connect("mongodb://localhost:27017/todoListDB", { useNewUrlParser: true })
+
+const itemsSchema = {
+	name: {
+		type: String,
+		required: true
+	}
+}
+
+const Item = mongoose.model("Item", itemsSchema)
+//create new doc
+
+const item1 = new Item({
+	name: "Welcome to your todo list app"
+})
+const item2 = new Item({
+	name: "Hit the + to add a new item"
+})
+const item3 = new Item({
+	name: "<-- Hit this to delete an item."
+})
+
+let defaultItems = [item1, item2, item3]
 
 app.get("/", (req, res) => {
 	// res.send("Welcome to my page");
-	let today = new Date()
-	let options = {
-		weekday: "long",
-		// year: "numeric",
-		month: "long",
-		day: "numeric"
-	}
-	let day = today.toLocaleDateString("en-US", options)
-
-	res.render("list", { kindOfDay: day, newListItem: items })
+	Item.find({}, (err, foundItems) => {
+		if (err) {
+			console.log(err)
+		} else {
+			if (foundItems.length === 0) {
+				Item.insertMany(defaultItems, (err) => {
+					if (err) {
+						console.log(err)
+					} else {
+						console.log("Successfully saved the item to the DB")
+					}
+				})
+				res.redirect("/")
+			} else {
+				res.render("list", { kindOfDay: "Today", newListItem: foundItems })
+			}
+		}
+	})
 })
 
 app.post("/", (req, res) => {
